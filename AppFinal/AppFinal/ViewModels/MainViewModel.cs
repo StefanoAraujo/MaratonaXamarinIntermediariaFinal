1b2ceb55d0f01f0560b447ce40026cca908b9572
Victor Angelo Marcorin <victor.am.ccomp@gmail.com>
2017-06-02 07:01:13 -0300

    Base View Model with binding

---
+++ AppFinal/AppFinal/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFinal.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _textProperty = "Initial Text";
+
+        public string TextProperty
+        {
+            get { return _textProperty; }
+            set
+            {
+                _textProperty = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
